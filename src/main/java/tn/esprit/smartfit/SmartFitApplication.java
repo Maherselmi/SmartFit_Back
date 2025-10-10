4accf40fadbb64a9156f9a1691d151080e558562
Maherselmi <selmimaher42@gmail.com>
2025-10-10 23:08:32 +0100

    push

---
+++ src/main/java/tn/esprit/smartfit/SmartFitApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.smartfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartFitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartFitApplication.class, args);
+    }
+
+}
